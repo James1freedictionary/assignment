0563ed4bc3e7c5ac2949e00e235b3eaeed0398dc
James1freedictionary <97018942+James1freedictionary@users.noreply.github.com>
2023-03-14 11:57:47 +0700

    Update transfer.sh

--- transfer.sh
+++ transfer.sh
@@ -3,7 +3,6 @@ transfer() {
   if [[ -d $1 ]]; then
     zip -qr $1.zip $1;
     curl --progress-bar --upload-file "$1.zip" https://transfer.sh/$(basename "$1.zip") | tee /dev/null;
-    echo '';
   elif [[ -f $1 ]]; then
     curl --progress-bar --upload-file "$1" https://transfer.sh/$(basename "$1") | tee /dev/null;
   else
