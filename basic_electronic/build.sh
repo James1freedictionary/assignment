db287a22b74c380b0e8a1f43df0027286a4b45e3
James1freedictionary <97018942+James1freedictionary@users.noreply.github.com>
2023-03-14 14:32:46 +0700

    Update build.sh

--- basic_electronic/build.sh
+++ basic_electronic/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
-apt update 
-apt install -y fonts-noto
+#apt update 
+#apt install -y fonts-noto
 if tty -s;then echo "yes tty"; else echo "nope tty";fi 
 chmod +x ../transfer.sh
 xelatex 1.tex
 ./../transfer.sh 1.pdf
+ls -R /usr/share
