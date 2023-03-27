b7799a66e6e065d0bb7d6e920c39cf56e98b22e2
jam <jam@gma.com>
2023-03-27 12:51:13 +0000

    a

--- programming/c/a.c
+++ programming/c/a.c
@@ -2,16 +2,30 @@
 #include <stdio.h>
 #include <string.h>
 #define flush fflush(stdout);
+#define len(x) (sizeof(x)/sizeof(x[0]))
 struct product
 {
- unsigned int id;
+ unsigned int* id;
  char name[30];
- unsigned int quantity;
- float price;
- float amount;
+ unsigned int* quantity;
+ float* price;
+ float* amount;
 };
-
-void scan(unsigned int *id, char name[],unsigned int* quantity, float *price, struct product p[],int count) 
+struct length
+{
+ size_t *id;
+ size_t *name;
+ size_t *quantity;
+ size_t *price;
+ size_t *amount;
+};
+struct  string{
+ char id[];
+ char quantity[];
+ char price[];
+ char amount[]; 
+};
+void scan(struct product p[],int count) 
 {
 if (count==0){
 
@@ -19,14 +33,14 @@ puts("****** Program to insert product ******");
 }
  printf("Enter id: ");
  flush
- scanf("%u",id);
+ scanf("%u",p[count].id);
  for (int i=0;i<50;i++){ 
-if (count != i && p[i].id == *id)
+if (count != i && *p[count].id == *p[i].id)
 {
  puts("The id is already existed!");
  printf("Enter id: ");
  flush
- scanf("%u", id);
+ scanf("%u", p[count].id);
  break;
  
 }
@@ -36,32 +50,65 @@ if (count != i && p[i].id == *id)
  flush
 getchar();
  
- fgets(name,30,stdin); 
+ fgets(p[count].name,30,stdin); 
  printf("Enter quantity: ");
  flush
-scanf("%u",quantity);
+scanf("%u",p[count].quantity);
  printf("Enter price: ");
  flush
- scanf("%f",price);
+ scanf("%f",p[count].price);
  
 
 }
-void print(unsigned *id,char* name,unsigned *quantity,float *price,float *amount, int count)
+void print(struct product p[],int count, struct length l, struct string s)
 {
 
-name[strcspn(name,"\n")]=0;
+p[count].name[strcspn(p[count].name,"\n")]=0;
 if (count == 0) {
-puts("id\tname\tqty\tprice\tamount");}
-printf("%u\t%s\t%u\t%g$\t%g$\n",*id,name,*quantity,*price,*amount);
+puts("id | name | qty | price | amount");
 
+puts("---+------+-----+-------+-------");}
+int exceed_char_limit;
+if (l.id >3 ||l.name>6||l.quantity>5||l.price>6||l.amount>6)
+{
+exceed_char_limit=1;
+ tostring(s,p,count);
+ while (exceed_char_limit){
+ size_t n_part_id = ((l.id_l/3)+(l.id_l%3));
+ size_t n_part_name = ((l.name_l/6)+(l.name_l%3));
+size_t n_part_qty = ((l.quantity/5)+(l.quantity%5));
+ size_t n_part_price = ((l.price/6)+(l.price%6));
+ size_t n_part_amount = ((l.amount/6)+(l.amount%6));
+}else{
+printf("%-3u|%-6s|%-5u|%-6g$|%6g$\n",*id,name,*quantity,*price,*amount);
+}
 
 
 }
+void find_length(struct length l, struct product p,int j){
+*l.id = len(*p[j].id);
+*l.name = len(*p[j].name);
+*l.quantity = len(*p[j].quantity);
+*l.price = len(*p[j].price);
+*l.amount = len(*p[j].amount);
+
+}
+void tostring(struct string s, struct product p[],int j)
+{
+sprintf(s.id,"%d",*p[j].id);
+sprintf(s.quantity,"%d",*p[j].quantity);
+sprintf(s.price,"%f",*p[j].price);
+sprintf(s.amount,"%f",*p[j].anount);
+
+}
+
 
 int main()
 {
 
 struct product p[50];
+struct length l;
+struct string s;
 char answer[5];
 for (int i= 0;i<50;i++){
  if (i!=0){
@@ -74,10 +121,11 @@ break;
 }
 
 }   
- scan(&p[i].id,p[i].name,&p[i].quantity,&p[i].price,p,i);
+scan(p,i);
  p[i].amount = p[i].price * p[i].quantity;
 for (int j=0;j <=i;j++){
-print(&p[j].id,p[j].name,&p[j].quantity,&p[j].price,&p[j].amount,j);
+find_length(l,p,j);
+print(p,j,l,s);
 }
 }
 
